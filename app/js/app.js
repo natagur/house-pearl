057ce5233a666fe5f94a8356647c187574662898
@natalia_gureva <gur.natalia.n@yandex.ru>
2022-03-31 21:20:01 +0500

    слайдеры и правки

---
+++ app/js/app.js
@@ -0,0 +1,73 @@
+function clickSlider(obj, newIndex){
+    obj.parents('.item').find('.slide-images > img').removeClass('active');
+    obj.parents('.item').find('.slide-images > img').eq(newIndex).addClass('active');
+
+    obj.parents('.item').find('.click-orange').removeClass('active');
+    obj.parents('.item').find('.click-orange').eq(newIndex).addClass('active');
+
+    clickSliderList();
+
+}
+function clickSliderList(){
+    let widthImg = parseInt($('.slider-naturele').css('width')) + 50;
+    let countImg = $('.slider-naturele').length;
+    console.log('-------------');
+    console.log(widthImg);
+    console.log(countImg);
+    
+    let left = (widthImg + 50) * $('.slider-naturele.active').index();
+    left = (widthImg * countImg) / 2 - left - 57;
+
+    console.log(left);
+    console.log((widthImg * countImg/2));
+    console.log($('.slider-naturele.active').index());
+    $('.slider-naturele').parent().css('left',left + 'px');
+}
+$(function(){
+    clickSliderList();
+
+    $(document).on('click', '.construction-choice-item', function(event){
+        $(this).parent().find('.construction-choice-item').removeClass('active');
+        $(this).addClass('active');
+
+        
+        $('.construction-choice-block-link .construction-choice-block').removeClass('active');
+        $('.construction-choice-block-link .construction-choice-block').eq($(this).index()).addClass('active');
+
+        event.preventDefault();
+    });
+
+    $(document).on('click', '.first-button', function(event){
+        
+        let activeIndex = $(this).parents('.item').find('.slide-images > img.active').index();
+        let countImg = $(this).parents('.item').find('.slide-images > img').length;
+        let newIndex = activeIndex - 1;
+
+        if (newIndex < 0) {
+            newIndex = countImg - 1;
+        }
+        clickSlider($(this), newIndex);
+
+        event.preventDefault();
+    });
+
+    $(document).on('click', '.second-button', function(event){
+        
+        let activeIndex = $(this).parents('.item').find('.slide-images > img.active').index();
+        let countImg = $(this).parents('.item').find('.slide-images > img').length;
+        let newIndex = activeIndex + 1;
+
+        if (newIndex > countImg -1) {
+            newIndex = 0;
+        }
+       
+        clickSlider($(this), newIndex);
+
+        event.preventDefault();
+    });
+
+    $(document).on('click', '.click-orange', function(event){
+        clickSlider($(this), $(this).index());
+    });
+})
+
